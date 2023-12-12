638aeae985c1132e96389ab04191c0dc902071b7
David Canto <david@davidcphoto.com>
2023-12-12 09:10:10 +0000

    Versão do Daniel

---
+++ changeman 1.ps1
@@ -0,0 +1,518 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+
+####################################################################################
+#  Definitions                                                                     #
+####################################################################################
+$user = 'XZ0670'
+#
+$lblMainWindowTxt = 'ChangeMan'
+$lblPackageTxt    = 'Package'
+$lblUserTxt       = 'User'
+$btnPullTxt       = 'Pull'
+$btnPushTxt       = 'Push'
+$btnExitTxt       = 'Exit'
+$btnListTxt       = 'List'
+$txtPackageTxt    = 'CORE'
+####################################################################################
+#                                 listPackageComponents                            #       
+####################################################################################
+function listPackageComponents() {
+
+    Write-Host " > listPackageComponents"
+    
+    $progressBar.Value = 1
+    $progressBar.Maximum = 9
+    $progressBar.Refresh    
+    
+    $listViewComponents.Items.Clear()    
+    $progressBar.Value = 2
+
+    if ($txtBoxPackage.Text.Length -ne 9){
+        $txtboxPackageNumber = $txtBoxPackage.Text.substring(4)
+        ##Write-Host $txtboxPackageNumber
+        $txtboxPackageNumberMax = $txtboxPackageNumber.padleft(6,'0')
+        ##Write-Host $txtboxPackageNumberMax
+        ##$txtboxPackage = $txtBoxPackage.Text.substring(0,4) + $txtboxPackageNumberMax
+        ##Write-Host $txtboxPackage                              
+        $packageNumber = $txtboxPackageNumberMax
+        $txtBoxPackage.Text = $txtBoxPackage.Text.substring(0,4) + $txtboxPackageNumberMax
+    }                                          
+    else{
+        $packageNumber = $txtBoxPackage.Text.substring($txtBoxPackage.Text.Length - 6)
+    }    
+    Write-Host "   | " $txtBoxPackage.Text
+
+    $progressBar.Value = 3
+    $progressBar.Refresh
+
+    $lstPrograms = @()
+    $lstCopybooks = @()
+    $lstUnloads = @()
+      
+    $progressBar.Value = 4
+    $progressBar.Refresh
+
+    $componentList = @(zowe zos-files list data-set "CMNP.MG1D.STG.CORE.#$packageNumber")
+    
+    $progressBar.Value = 5
+    $progressBar.Refresh
+
+    for ($i = 0; $i -lt $componentList.Length; $i++) {
+        switch ($componentList[$i].substring($componentList[$i].Length - 3)) {
+            'SRC' {
+                $lstPrograms = @(zowe zos-files list all-members $componentList[$i])
+            }
+            'CPY' {
+                $lstCopybooks = @(zowe zos-files list all-members $componentList[$i])
+            }
+            'UNL' {
+                $lstUnloads = @(zowe zos-files list all-members $componentList[$i])
+            }
+        }
+    }
+
+    $progressBar.Value = 6
+    $progressBar.Refresh
+
+    for ($i = 0; $i -lt $lstPrograms.Length; $i++) {
+        if ($lstPrograms[$i] -ne "") {            
+            $componentItem = New-Object System.Windows.Forms.ListViewItem
+            $componentItem.Text = $lstPrograms[$i]
+            $componentItem.SubItems.Add("SRC")
+            $componentItem.SubItems.Add("-")
+            $componentItem.SubItems.Add("")
+            $listViewComponents.Items.Add($componentItem)
+        }
+    }
+
+    $progressBar.Value = 7
+    $progressBar.Refresh
+
+    for ($i = 0; $i -lt $lstCopybooks.Length; $i++) {
+        if ($lstCopybooks[$i] -ne "") {
+            $componentItem = New-Object System.Windows.Forms.ListViewItem
+            $componentItem.Text = $lstCopybooks[$i] 
+            $componentItem.SubItems.Add("CPY")
+            $componentItem.SubItems.Add("-")
+            $componentItem.SubItems.Add("")
+            $listViewComponents.Items.Add($componentItem)
+        }
+    }
+
+    $progressBar.Value = 8
+    $progressBar.Refresh
+
+    for ($i = 0; $i -lt $lstUnloads.Length; $i++) {
+        if ($lstPrograms[$i] -ne "") {
+            $componentItem = New-Object System.Windows.Forms.ListViewItem
+            $componentItem.Text = $lstUnloads[$i]
+            $componentItem.SubItems.Add("UNL")
+            $componentItem.SubItems.Add("-")
+            $componentItem.SubItems.Add("")
+            $listViewComponents.Items.Add($componentItem)
+        }
+    }
+
+    $progressBar.Value = 9
+    $progressBar.Refresh
+
+    refreshList
+}
+####################################################################################
+#                                   pushComponent                                  
+####################################################################################
+function pushComponent() {
+
+    Write-Host " > pushComponent"
+
+    $progressBar.Maximum = 5
+    $packageNumber = $txtBoxPackage.Text.Substring(4, 6)
+    $userName = $txtBoxUser.Text
+
+
+    for ($i = 0; $i -lt $listViewComponents.SelectedItems.Count; $i++) {
+        $componentName= $listViewComponents.SelectedItems.Item($i).SubItems[0].Text
+        $componentExtension = $listViewComponents.SelectedItems.Item($i).SubItems[1].Text
+        $listViewComponents.SelectedItems.Item($i).SubItems[2].Text = '-'
+        $listViewComponents.SelectedItems.Item($i).SubItems[3].Text = ''
+        Write-Host "   | " $componentName'.'$componentExtension                   
+        $progressBar.Value = 1
+
+        if ($componentExtension -eq "cpy") {
+            $componentType = "COPY"
+            $job = "//" + $userName + "PS JOB (851,985,995),'Push - $componentName',
+//         MSGCLASS=X,CLASS=D
+//*
+//*
+//JOBLIB   DD  DISP=SHR,
+//             DSN=SYSU.MG1D.CMNZMF.PROD.LOAD
+//         DD  DISP=SHR,
+//             DSN=SYSU.MG1D.SERCOMC.PROD.LOAD
+//         DD  DISP=SHR,
+//             DSN=SYSP.CMNZMF.LOAD
+//         DD  DISP=SHR,
+//             DSN=SYSP.SERCOMC.LOAD
+//*)IM CMNSRVC
+//*
+//CMNSRVC EXEC PGM=CMNVSRVC,
+//             COND=(4,LT),
+//             PARM='SUBSYS=P,USER=$userName'
+//SER#PARM DD  DISP=SHR,DSN=SYSU.MG1D.SERCOMC.PROD.V8R2P05.TCPIPORT
+//SERRPLY  DD  SYSOUT=*,
+//             DCB=(RECFM=VB,LRECL=4080,BLKSIZE=0)
+//SERPRINT DD  SYSOUT=*                             TRACE OUTPUT
+//SYSUDUMP DD  SYSOUT=*
+//SYSIN    DD  *
+OBJ=CMPONENT,MSG=CHECKIN,PKN=CORE$packageNumber,
+LTP=$componentExtension,CKF=1,SLT=6,
+LIB=$userName.LIB.$componentType,
+LOK=N,SPV=Y,CCD=                               ,
+SUP=N,
+TMN=$componentName,
+CNT=00001"
+        }
+        elseif ($componentExtension -eq "src") {
+            $componentType = "SOURCE"
+            $sourceType = ""
+
+            if ($componentName.substring(2, 1) -eq "I") {
+
+                if ($componentName.substring(5, 3) -in ("020" -or "040" -or "060" -or "080")) {
+                    # online
+                    $sourceType = "UO1=  Y       ,UO2=          ,UHS=N,"
+                }
+                else {
+                    # Rotina online
+                    $sourceType = "Rotina Online"
+                }
+
+            }
+            elseif ($componentName.substring(2, 1) -eq "B") {
+                # batch
+                $sourceType = "UO1=        Y ,UO2=          ,UHS=N,"
+            }
+            elseif ($componentName.substring(2, 1) -eq "R") {
+                # Rotina batch
+                $sourceType = "Rotina Batch"
+            }
+            else {
+                $sourceType = $componentName.substring(2, 1)
+            }
+
+            $job = "//" + $userName + "PS JOB (851,985,995),'Push - $componentName',
+//         MSGCLASS=X,CLASS=D
+//*
+//*        -----------------------------------------------------------
+//*        IEBCOPY: COPIA DE ELEMENTO DE UM UTILIZADOR PARA O PACOTE
+//*        -----------------------------------------------------------
+//*        -----------------------------------------------------------
+//JOBLIB   DD  DISP=SHR,
+//             DSN=SYSU.MG1D.CMNZMF.PROD.LOAD
+//         DD  DISP=SHR,
+//             DSN=SYSU.MG1D.SERCOMC.PROD.LOAD
+//         DD  DISP=SHR,
+//             DSN=SYSP.CMNZMF.LOAD
+//         DD  DISP=SHR,
+//             DSN=SYSP.SERCOMC.LOAD
+//*)IM CMNSRVC
+//*
+//DELFILE EXEC PGM=IDCAMS
+//SYSPRINT DD   SYSOUT=*
+//SYSIN    DD   *
+  DELETE (CMNP.TEMP.CORE.#$packageNumber.$componentName.S1) NONVSAM
+  SET MAXCC = 0
+/*
+//*
+//CMNSRVC EXEC PGM=CMNVSRVC,
+//             COND=(4,LT),
+//             PARM='SUBSYS=P,USER=$userName'
+//SER#PARM DD  DISP=SHR,DSN=SYSU.MG1D.SERCOMC.PROD.V8R2P05.TCPIPORT
+//SERRPLY  DD  SYSOUT=*,
+//             DCB=(RECFM=VB,LRECL=4080,BLKSIZE=0)
+//SERPRINT DD  DISP=(,CATLG),
+//             DSN=CMNP.TEMP.CORE.#$packageNumber.$componentName.S1,
+//             UNIT=SYSDA,SPACE=(CYL,(5,5),RLSE),
+//             DCB=(RECFM=FBA,LRECL=133,BLKSIZE=0)
+//SYSUDUMP DD  SYSOUT=*
+//SYSIN    DD  *
+OBJ=CMPONENT,MSG=CHECKIN,PKN=CORE$packageNumber,
+LTP=$componentExtension,CKF=1,SLT=6,
+LIB=$userName.LIB.$componentType,
+LOK=N,SPV=Y,CCD=                               ,
+SUP=N,
+TMN=$componentName,
+CNT=00001
+OBJ=CMPONENT,MSG=BUILD,PKN=CORE$packageNumber,
+LTP=$componentExtension,PRC=BMPCOBE ,LNG=COBOLE  ,
+$sourceType
+DB2=Y,DBS=DB2D,DBL=SYS1.DB2.SDSNLOAD                           ,
+JC1=//" + $userName + "B JOB ,'Compilar CORE$packageNumber',MSGCLASS=X,
+JC2=//         NOTIFY=XZ0670,CLASS=N
+JC3=//*
+JC4=//*
+SUP=N,
+UPN=CMNUSR01,
+IJN=N,
+DSN=CMNP.MG1D.STG.CORE.#$packageNumber.$componentExtension,
+CMP=$componentName,
+CNT=00001"
+        }
+        elseif ($componentExtension -eq "unload") {
+            $componentType = "UNLOAD"
+        }
+        $progressBar.Value = 2
+
+        $job | Out-file -encoding ASCII -noNewline -FilePath "$env:TEMP\Job.JCL"
+        
+        $progressBar.Value = 3
+        
+        jobSubmit 'Push'             
+        
+        $progressBar.Value = 5
+    }
+  
+    refreshList
+}
+
+####################################################################################
+#                                   pullComponent                                  #
+####################################################################################
+function pullComponent() {
+
+    Write-Host " > pullComponent"
+
+    $progressBar.Maximum = 6
+    $packageNumber = $txtBoxPackage.Text.Substring(4, 6)
+
+    for ($i = 0; $i -lt $listViewComponents.SelectedItems.Count; $i++) {       
+        
+        $componentName= $listViewComponents.SelectedItems.Item($i).SubItems[0].Text
+        $componentExtension = $listViewComponents.SelectedItems.Item($i).SubItems[1].Text
+        $listViewComponents.SelectedItems.Item($i).SubItems[2].Text = '-'
+        $listViewComponents.SelectedItems.Item($i).SubItems[3].Text = ''
+        Write-Host "   | " $componentName'.'$componentExtension                   
+
+        $progressBar.Value = 1
+
+        if ($componentExtension -eq "cpy") {
+            $componentType = "COPY"
+        }
+        elseif ($componentExtension -eq "src") {
+            $componentType = "SOURCE"
+        }
+        elseif ($componentExtension -eq "unload") {
+            $componentType = "UNLOAD"
+        }
+
+        $progressBar.Value = 2
+
+        $userName = $txtBoxUser.Text
+
+        $progressBar.Value = 3
+
+        $job = "//" + $userName + "PL JOB (851,985,995),'Pull - $componentName',
+//         MSGCLASS=X,CLASS=D
+//*
+//*        -----------------------------------------------------------
+//*        IEBCOPY: COPIA DE ELEMENTO DE UM PACOTE PARA O UTILIZADOR
+//*        -----------------------------------------------------------
+//*        -----------------------------------------------------------
+//COPIA    EXEC PGM=IEBCOPY,COND=(0,NE)
+//SYSPRINT DD SYSOUT=*
+//IN       DD DISP=SHR,DSN=CMNP.MG1D.STG.CORE.#$packageNumber.$componentExtension
+//OUT      DD DISP=SHR,DSN=$userName.LIB.$componentType
+//SYSIN    DD *
+  COPY     INDD=IN,OUTDD=OUT
+  SELECT   MEMBER=(($componentName,,R))
+//*"
+        $progressBar.Value = 4
+        $job | Out-file -encoding ASCII -noNewline -FilePath "$env:TEMP\Job.JCL"
+        $progressBar.Value = 5
+
+        jobSubmit 'Pull'      
+        
+        $progressBar.Value = 6
+    }
+
+    refreshList
+
+}
+####################################################################################
+#                                   jobSubmit                                      #
+####################################################################################
+function jobSubmit() {                                                                                                          
+    Param($jobType)
+
+    $outputJCL = @(zowe jobs submit local-file "$env:TEMP\Job.JCL" --response-format-json --view-all-spool-content --reject-unauthorized false)  
+    $listViewComponents.SelectedItems.Item($i).SubItems[2].Text = $jobType + ' Ok'
+    $progressBar.Value = 4
+
+    Write-Host   $outputJCL
+
+    $data = $outputJCL | ConvertFrom-Json       
+    
+    $data.stdout
+
+    if ($jobType -eq 'Push'){
+        $jobRC = $userName + "PS ENDED - RC="    
+    }else
+    {
+        $jobRC = $userName + "PL ENDED - RC="}    
+    
+    $jobRCPosition = $data.stdout.LastIndexOf($jobRC)
+    $returnCode = $data.stdout.Substring($jobRCPosition+$jobRC.Length,4)
+    $listViewComponents.SelectedItems.Item($i).SubItems[3].Text = $returnCode
+    if ($returnCode -ne '0000'){
+        $listViewComponents.SelectedItems.Item($i).SubItems[2].Text = $jobType + ' Not Ok'
+    }
+}    
+####################################################################################
+#                       refreshList                                                #
+####################################################################################
+function refreshList {        
+    $progressBar.Value = 0
+    $progressBar.Refresh
+
+    $listViewComponents.SelectedItems.Clear()
+    $listViewComponents.Refresh()        
+    
+    $listViewComponents.Columns[0].Width = -1
+    $listViewComponents.Columns[1].Width = -2 
+    $listViewComponents.Columns[2].Width = -2 
+    $listViewComponents.Columns[3].Width = -2 
+}
+####################################################################################
+#                        main                                                      #
+####################################################################################
+function mainWindow {
+
+    Write-Host "mainWindow"
+
+    $frmMainWindow = New-Object System.Windows.Forms.Form
+    $frmMainWindow.Text = $lblMainWindowTxt
+    $frmMainWindow.Size = New-Object System.Drawing.Size(300, 300)
+    $frmMainWindow.FormBorderStyle = "FixedDialog"
+    $frmMainWindow.MaximizeBox = $false
+    $frmMainWindow.StartPosition = 'CenterScreen'
+
+
+    $lblPackage = New-Object System.Windows.Forms.Label
+    $lblPackage.Location = New-Object System.Drawing.Point(10, 10)
+    $lblPackage.Size = New-Object System.Drawing.Size(60, 20)
+    $lblPackage.Text = $lblPackageTxt
+    $frmMainWindow.Controls.Add($lblPackage)
+
+
+    $txtBoxPackage = New-Object System.Windows.Forms.TextBox
+    $txtBoxPackage.Location = New-Object System.Drawing.Point(70, 10)
+    $txtBoxPackage.Size = New-Object System.Drawing.Size(120, 20)
+    $txtBoxPackage.Text = $txtPackageTxt
+    $txtBoxPackage.MaxLength = 10
+    $txtBoxPackage.CharacterCasing = 'Upper'
+    $txtBoxPackage.Add_KeyDown({
+            if ($_.KeyCode -eq [System.Windows.Forms.Keys]::Enter) {
+                #logic
+                listPackageComponents
+            }
+        })
+    $frmMainWindow.Controls.Add($txtBoxPackage)
+
+
+    $btnList = New-Object System.Windows.Forms.Button
+    $btnList.Location = New-Object System.Drawing.Point(200, 10)
+    $btnList.Size = New-Object System.Drawing.Size(75, 23)
+    $btnList.Text = $btnListTxt
+    $btnList.Add_Click({ listPackageComponents })
+    $frmMainWindow.Controls.Add($btnList)
+
+
+    $lblUser = New-Object System.Windows.Forms.Label
+    $lblUser.Location = New-Object System.Drawing.Point(10, 30)
+    $lblUser.Size = New-Object System.Drawing.Size(60, 20)
+    $lblUser.Text = $lblUserTxt 
+    $frmMainWindow.Controls.Add($lblUser)
+
+
+    $txtBoxUser = New-Object System.Windows.Forms.TextBox
+    $txtBoxUser.Location = New-Object System.Drawing.Point(70, 30)
+    $txtBoxUser.Size = New-Object System.Drawing.Size(120, 20)
+    $txtBoxUser.CharacterCasing = 'Upper'
+    $txtBoxUser.Text = $User
+    $txtBoxUser.MaxLength = 6
+    $txtBoxUser.Add_KeyDown({
+            if ($_.KeyCode -eq [System.Windows.Forms.Keys]::Enter) {
+                #logic
+                listPackageComponents
+            }
+        })
+    $frmMainWindow.Controls.Add($txtBoxUser)
+
+
+    $listViewComponents = New-Object System.Windows.Forms.ListView 
+    $listViewComponents.Location = New-Object System.Drawing.Point(10, 50)
+    $listViewComponents.Size = New-Object System.Drawing.Size(265, 170)
+    $listViewComponents.CheckBoxes=$false
+    $listViewComponents.AutoArrange=$true
+    $listViewComponents.GridLines=$true
+    $listViewComponents.MultiSelect=$true
+    $listViewComponents.AllowColumnReorder = $true
+    $listViewComponents.View = "Details"    
+    $listViewComponents.Columns.Add("Name")
+    $listViewComponents.Columns[0].Width = -2
+    $listViewComponents.Columns.Add("Type")
+    $listViewComponents.Columns[1].Width = -2
+    $listViewComponents.Columns.Add("Status")
+    $listViewComponents.Columns[2].Width = -2
+    $listViewComponents.Columns.Add("RC")
+    $listViewComponents.Columns[3].Width = -2
+    $frmMainWindow.Controls.Add($listViewComponents)  
+
+
+    $btnPush = New-Object System.Windows.Forms.Button
+    $btnPush.Location = New-Object System.Drawing.Point(85, 230)
+    $btnPush.Size = New-Object System.Drawing.Size(75, 23)
+    $btnPush.Text = $btnPushTxt
+    $btnPush.Add_Click({ pushComponent })
+    $frmMainWindow.Controls.Add($btnPush)
+
+
+    $btnPull = New-Object System.Windows.Forms.Button
+    $btnPull.Location = New-Object System.Drawing.Point(10, 230)
+    $btnPull.Size = New-Object System.Drawing.Size(75, 23)
+    $btnPull.Text = $btnPullTxt
+    $btnPull.Add_Click({ pullComponent })
+    $frmMainWindow.Controls.Add($btnPull)
+
+
+    $btnExit = New-Object System.Windows.Forms.Button
+    $btnExit.Location = New-Object System.Drawing.Point(200, 230)
+    $btnExit.Size = New-Object System.Drawing.Size(75, 23)
+    $btnExit.Text = $btnExitTxt
+    $btnExit.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $frmMainWindow.CancelButton = $btnExit
+    $frmMainWindow.Controls.Add($btnExit)
+
+
+    $progressBarAmount = 100
+    $progressBar = New-Object System.Windows.Forms.progressBar
+    $progressBar.Minimum = 0
+    $progressBar.Maximum = $progressBarAmount
+    $progressBar.Location = new-object System.Drawing.Size(10, 215)
+    $progressBar.size = new-object System.Drawing.Size(265, 10)
+    $frmMainWindow.Controls.Add($progressBar)
+
+    $frmMainWindow.Topmost = $true
+    $frmMainWindow.ShowDialog()
+
+}
+####################################################################################
+#                                main                                              #
+####################################################################################
+
+Write-Host "begin"
+
+mainWindow
+
+Write-Host "end"
